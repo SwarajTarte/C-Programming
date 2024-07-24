a434e91a222d12b707df2593c8e99442bcf36cb0
Swaraj Tarte <swaraj.codes@gmail.com>
2024-07-24 16:37:28 +0530

    6th Program

---
+++ Simple_Interest.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    float principal, years, rate, interest;
+
+    printf("Enter Principal Amount in Rs.: \n");
+    scanf("%f", &principal);
+
+    printf("Enter tenure/no. of years: \n");
+    scanf("%f", &years);
+
+    printf("Enter Rate of Interest in %%: \n");
+    scanf("%f", &rate);
+
+    interest = (principal * rate * years) / 100;
+
+    printf("The total simple interestn is of Rs. %f. \n", interest);
+
+    printf("Total amount with interest is Rs. %f", interest + principal);
+
+    return 0;
+}
