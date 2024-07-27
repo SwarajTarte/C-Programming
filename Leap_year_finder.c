7d7212ecc9526c42930cedfcdc026f9c6846132c
Swaraj Tarte <swaraj.codes@gmail.com>
2024-07-27 17:57:35 +0530

    11th Program

--- Leap_year_finder.c
+++ Leap_year_finder.c
@@ -1,5 +1,11 @@
 // This code will tell you whether the year entered by you is a leap year or not.
 
+/*
+    Program : Leap_year_finder.c
+    Author  : Swaraj Tarte
+    Date    : 27 July 2024
+*/
+
 #include <stdio.h>
 
 int main()
