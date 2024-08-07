cdcd2a34b9bc1b1928a1ae377a9bedeec234eccf
Swaraj Tarte <swaraj.codes@gmail.com>
2024-08-07 12:21:12 +0530

    29th Program

---
+++ Product.c
@@ -0,0 +1,25 @@
+// This code will give you the product of the entered numbers by using call by reference.
+
+#include <stdio.h>
+
+int multiply(int *a, int *b);
+
+int multiply(int *a, int *b)
+{
+    return *a * *b;
+}
+
+int main()
+{
+    int i, j;
+
+    printf("Enter 1st numbers: \n ");
+    scanf("%d", &i);
+
+    printf("Enter 2nd numbers: \n ");
+    scanf("%d", &j);
+
+    printf("The product of %d and %d is %d", i, j, multiply(&i, &j));
+
+    return 0;
+}
